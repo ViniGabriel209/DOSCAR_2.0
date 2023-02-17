778b3d8dff57aa46b6b2f72a8a7ff4414e2bd07f
Vinicius Gabriel <121624375+ViniGabriel209@users.noreply.github.com>
2023-02-17 10:34:45 -0300

    Update offset.py

--- offset.py
+++ offset.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 vbm, cbm = np.genfromtxt('bandedges.txt', unpack = True)
 
-layer = [-11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+layer = [-18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 
 def bandalignment():
     
